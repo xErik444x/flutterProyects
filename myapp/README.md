210722be516e5041ae4fb46b682f29bd7a2f0194
erik Schwerdt <erikschwerdt18@gmail.com>
2020-09-10 17:52:06 -0300

    añadir assets y mostrarlos

--- myapp/README.md
+++ myapp/README.md
@@ -1,3 +1,14 @@
 # myapp
 > Para crear un materialize app, mateapp
 > para crear un stless widget, stlw
+
+## añadir assets
++ Crear una carpeta con el nombre assets
++ Añadir las imagenes
++ ir al archivo pubspec.yaml y buscar la linea:
+> assets:
++ Descomentarla y agregar la direccion del asset en cuestion
+<br>
+
+        assets:
+        - assets/img/pinguino.jpg
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,22 +13,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key}) : super(key: key);
-List<String> names = ["hola","jose","erik","cosas","palabras","jose","erik","cosas","palabras","jose","erik","cosas","palabras","jose","erik","cosas","palabras","jose","erik","cosas","palabras"];
+  MyHomePage({
+    Key key
+  }): super(key: key);
+  List < String > names = ["hola", "jose", "erik", "cosas", "palabras", "jose", "erik", "cosas", "palabras", "jose", "erik", "cosas", "palabras", "jose", "erik", "cosas", "palabras", "jose", "erik", "cosas", "palabras"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('ListView'),
-        ),
-        body:  ListView.builder( //apunta a una lista
-          itemCount: 30,
-          itemBuilder: (BuildContext context, int index){
-           return Image.network("https://picsum.photos/id/$index/400/400",fit: BoxFit.cover,);
-          }
-          )
-      );
+      appBar: AppBar(
+        title: Text('ListView'),
+      ),
+      body: ListView(
+        children: [
+          Image.asset("assets/img/pinguino1.jpg", fit: BoxFit.cover),
+          Image.asset("assets/img/pinguino2.jpg", fit: BoxFit.cover),
+          Image.asset("assets/img/pinguino3.jpg", fit: BoxFit.cover)
+        ],
+      )
+    );
   }
 }
